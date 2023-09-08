db91041bc40fc01c85c7547222a630b39228e7c7
william <laurencius.william@gmail.com>
2023-09-08 18:18:28 +0700

    update background

--- pages/menu.js
+++ pages/menu.js
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import Page from "@/components/page";
-import bg1 from "../public/bg1.jpg";
+import bg7 from "../public/bg7.jpg";
 
 export default function Menu() {
   return (
     <Page>
       <div className="flex flex-col items-center justify-between">
         <div className="relative w-full"></div>
-        <Image src={bg1} className="w-screen h-screen"></Image>
+        <Image src={bg7} className="w-screen h-screen"></Image>
       </div>
     </Page>
   );
